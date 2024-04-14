42aebd4db8c996426f936963120c848bed7e31a9
Iryna Kakalets <irinakakalec@gmail.com>
2024-04-14 08:40:45 +0200

    hotove

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log('funguju');
+
+const shirt = document.querySelector('svg');
+
+document.querySelector('.colors').innerHTML += `
+<div class="color">
+            <div class="color-box color-box--black"></div>
+            <p class="color__name">černá</p>
+          </div>
+          <div class="color">
+            <div class="color-box color-box--red"></div>
+            <p class="color__name">červená</p>
+          </div>
+          <div class="color">
+            <div class="color-box color-box--blue"></div>
+            <p class="color__name">nebesky modrá</p>
+          </div>`;
+
+const colors = document.querySelectorAll('.color-box');
+
+for (let i = 0; i < colors.length; i++) {
+  colors[i].addEventListener('click', () => {
+    shirt.style = `fill : ${getComputedStyle(colors[i]).backgroundColor}`;
+  });
+}
+
+const productOrder = () => {
+  const productContainerElm = document.querySelector('.product');
+  productContainerElm.textContent = 'Objednáno!';
+};
+
+const orderIt = document.querySelector('#product__order');
+orderIt.addEventListener('click', productOrder);
